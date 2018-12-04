e4a7758dff8b0e58064343c112122cf52c1132d9
niaoge <78493244@qq.com>
2018-12-05 04:55:57 +0800

    mobile 时间 ok

--- src/utils/UIUtil.tsx
+++ src/utils/UIUtil.tsx
@@ -16,53 +16,140 @@ import {WrappedFormUtils} from "antd/lib/form/Form";
 
 namespace UIUtil {
 
-  export const createFieldProps = (formItemConfig: FormItemConfig,formPropsUtils?) => {
-    formPropsUtils =formPropsUtils || formItemConfig.formPropsUtils;
+  export const createFieldProps = (formItemConfig: FormItemConfig, formPropsUtils?) => {
+    formPropsUtils = formPropsUtils || formItemConfig.formPropsUtils;
     return formPropsUtils.getFieldProps(formItemConfig.name, formItemConfig.config);
   }
 
+  export interface InputEditorProps extends Partial<InputItemProps>, FormItemEditorProps {
+  }
 
-  export const buildInputEditor = (props) => {
-    console.log(props)
-    const {formItemConfig, ...othersProps} = props;
+  export const BuildInputEditor = (props?: InputEditorProps) => {
+    const {formItemConfig, ...customs} = props;
     return (
-      <Input
+      <InputItem
         key={formItemConfig.name}
-        {...othersProps}
+        {...customs}
         {...UIUtil.createFieldProps(formItemConfig)}
+        value={formItemConfig.value}
       >
-      </Input>
+        {props.children || formItemConfig.label}
+      </InputItem>
     )
   }
 
-  export const buildTimeStampEditor = (props) => {
-    const formItemConfig: FormItemConfig = this.formItemConfig;
-    return (<DatePicker showTime locale={locale} format={formItemConfig.format} {...props}/>)
+  export interface PasswordEditorProps extends InputEditorProps {
   }
 
-  export const buildTimePickerEditor = (props) => {
-    const formItemConfig: FormItemConfig = this.formItemConfig;
-    return (<TimePicker format={formItemConfig.format} {...props}/>)
+  export const BuildPasswordEditor = (props?: PasswordEditorProps) => {
+    props['type'] = 'password';
+    return BuildInputEditor(props);
   }
 
-  export const buildDatePickerEditor = (props) => {
+  export interface HiddenEditorProps extends InputEditorProps {
+  }
+
+  export const BuildHiddenEditor = (props?: HiddenEditorProps) => {
+    const {formItemConfig, ...customs} = props;
+    return (
+      <InputItem
+        type="hidden"
+        key={formItemConfig.name}
+        {...customs}
+        {...UIUtil.createFieldProps(formItemConfig)}
+        value={formItemConfig.value}
+      >
+      </InputItem>
+    )
+  }
+
+  export interface TextAreaEditorProps extends Partial<TextareaItemProps>, FormItemEditorProps {
+
+  }
+
+  export const BuildTextAreaEditor = (props?: TextAreaEditorProps) => {
+    const {formItemConfig, ...customs} = props;
+    return (
+      <TextareaItem
+        title={formItemConfig.label}
+        key={formItemConfig.name}
+        autoHeight
+        {...customs}
+        {...UIUtil.createFieldProps(formItemConfig)}
+        value={formItemConfig.value}
+      >
+        {props.children || formItemConfig.label}
+      </TextareaItem>
+    )
+  }
+
+  export interface TimeStampEditorProps extends Partial<DatePickerProps>, FormItemEditorProps {
+
+  }
+
+
+  export const BuildTimeStampEditor = (props?: TimeStampEditorProps) => {
+    const {formItemConfig, ...customs} = props;
+    return (
+      <DatePicker
+        showTime
+        locale={locale}
+        format={formItemConfig.format}
+        {...customs}
+      />
+    )
+  }
+
+  export interface DatePickerEditorProps extends DatePickerProps, FormItemEditorProps {
+  }
+
+  export const BuildDatePickerEditor = (props?: DatePickerEditorProps) => {
     const formItemConfig: FormItemConfig = this.formItemConfig;
     return (<DatePicker locale={locale} format={formItemConfig.format} {...props}/>)
   }
 
-  export const buildEnumEditor = (props) => {
+  export interface TimePickerEditorProps extends FormItemEditorProps {
+  }
+
+  export const BuildTimePickerEditor = (props?: TimePickerEditorProps) => {
     const formItemConfig: FormItemConfig = this.formItemConfig;
+    return (<TimePicker format={formItemConfig.format} {...props}/>)
+  }
+
+
+  export interface EnumEditorProps extends Partial<AbstractPickerProps>, FormItemEditorProps {
+  }
+
+  export const BuildEnumEditor = (props?: EnumEditorProps) => {
+    const {formItemConfig, ...customs} = props;
     const muti = formItemConfig.isArray ? {mode: "multiple"} : null;
+    const optionProps = getOptionProps(formItemConfig.options);
+    let initialValue = formItemConfig.config.initialValue;
+    if (initialValue != null && !(initialValue instanceof Array)) {
+      formItemConfig.config.initialValue = [initialValue];
+      formItemConfig.value = [initialValue];
+    }
     return (
-      <Select {...muti} style={{width: 150}} {...props}>
-        {UIUtil.makeSelectOptions(formItemConfig.options)}
-      </Select>
+      <Picker
+        data={optionProps}
+        title={formItemConfig.label}
+        extra="请选择"
+        {...customs}
+        {...UIUtil.createFieldProps(formItemConfig)}
+        value={formItemConfig.value}
+      >
+        {props.children || <List.Item arrow="down">{formItemConfig.label}</List.Item>}
+      </Picker>
     )
   }
 
-  export const buildImageEditor = (props) => {
+  export interface ImageEditorProps extends FormItemEditorProps {
+
+  }
+
+  export const BuildImageEditor = (props: ImageEditorProps) => {
     const formItemConfig: FormItemConfig = this.formItemConfig;
-    return (<Input {...props}/>)
+    return (<InputItem {...props}/>)
   }
 
   export const buildFormItem = (formItemConfig: FormItemConfig, wrappedForm: WrappedFormUtils, formItemProps: FormItemProps) => {
@@ -98,8 +185,8 @@ namespace UIUtil {
   export const makeSelectOptions = (options: Options = {}) => {
     const result = Object.values(options).map((option, key) => {
       return (
-        <Select.Option value={option.value} key={key}>{option.title || option.value}
-        </Select.Option>
+        <option value={option.value} key={key}>{option.title || option.value}
+        </option>
       )
     });
     return result;
@@ -109,3 +196,4 @@ namespace UIUtil {
 }
 
 export default UIUtil;
+
