67397e5fa3743b0c295ff865cbd722f10d6ccca5
niaoge <78493244@qq.com>
2019-01-15 02:57:57 +0800

    readme

--- src/intergrade/configs/tradeCms-statesAlias.ts
+++ src/intergrade/configs/tradeCms-statesAlias.ts
@@ -4,20 +4,20 @@
  *  created by [stategen.progen] ,do not edit it manually otherwise your code will be override by next call progen,
  *  由 [stategen.progen]代码生成器创建，不要手动修改,否则将在下次创建时自动覆盖
  */
-import {RoleState} from "../interfaces/RoleFaces";
-import {UserState} from "../interfaces/UserFaces";
-import {HomeState} from "../interfaces/HomeFaces";
-import {LoginState} from "../interfaces/LoginFaces";
-import {Bbs_topicState} from "../interfaces/Bbs_topicFaces";
 import {AppState} from "../interfaces/AppFaces";
+import {Bbs_topicState} from "../interfaces/Bbs_topicFaces";
+import {RoleState} from "../interfaces/RoleFaces";
+import {HomeState} from "../interfaces/HomeFaces";
+import {UserState} from "../interfaces/UserFaces";
+import {LoginState} from "../interfaces/LoginFaces";
 import {User_$userIdState} from "../interfaces/User_$userIdFaces";
 
 export default interface StatesAlias {
-  role: RoleState;
-  user: UserState;
-  home: HomeState;
-  login: LoginState;
-  bbs_topic: Bbs_topicState;
   app: AppState;
+  bbs_topic: Bbs_topicState;
+  role: RoleState;
+  home: HomeState;
+  user: UserState;
+  login: LoginState;
   user_$userId: User_$userIdState;
 }
